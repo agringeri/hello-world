069aa2983a02c6290cc9ec2b46dcb057d8a86c2c
Anthony <antgringeri@gmail.com>
2019-08-21 15:55:37 -0400

    added python routing for different paths

--- server.py
+++ server.py
@@ -1,16 +1,21 @@
 from flask import Flask
 
 PORT = 8000
-MESSAGE = "Hello, world!\n"
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def root():
-    result = MESSAGE.encode("utf-8")
+    result = "Hello, world!\n".encode("utf-8")
     return result
 
+@app.route("/user/<username>")
+def show_user(username):
+    return ("Hello, %s\n" % username).encode("utf-8")
+
+@app.route("/user/<username>/<message>")
+def show_user_and_message(username, message):
+    return ("Hello, %s\nHere's a message: %s\n" % (username, message)).encode("utf-8")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=PORT)
